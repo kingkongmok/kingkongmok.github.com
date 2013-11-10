140d1ebb39e2097066eff1ed869106ff2679cdcf
kingkongmok <kingkongmok@gmail.com>
2013-11-10 21:57:35 +0800

    a

---
+++ testSourceBash.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl 
+#===============================================================================
+#
+#         FILE: testSourceBash.pl
+#
+#        USAGE: ./testSourceBash.pl  
+#
+#  DESCRIPTION: test source function.
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: Kingkong Mok (), kingkongmok AT gmail DOT com
+#      COMPANY: 
+#      VERSION: 1.0
+#      CREATED: 11/10/2013 09:53:04 PM
+#     REVISION: ---
+#===============================================================================
+
+#use strict;
+#use warnings;
+#
+system "source /tmp/test.sh" ;
+
+print $NAME ;
