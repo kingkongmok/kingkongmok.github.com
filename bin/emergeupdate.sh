acde0b3c67ab78139b52050fcfaf9877255e286f
kingkongmok <kingkongmok@gmail.com>
2014-09-24 21:16:08 +0800

    add preserver @world step

--- bin/emergeupdate.sh
+++ bin/emergeupdate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 sudo nice -n 19 emerge --sync && \
 sudo nice -n 19 emerge --update --deep --with-bdeps=y --newuse @world && \
+sudo nice -n 19 emerge @preserved-rebuild \
 sudo nice -n 19 emerge --depclean && \
 sudo nice -n 19 revdep-rebuild
