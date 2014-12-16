bf569bafb17a3569646eeab3302fba1a4ea9726b
kingkongmok <kingkongmok@gmail.com>
2014-12-16 09:25:31 +0800

    cleaning tool for Gentoo distfiles and binary packages.

--- bin/emergeupdate.sh
+++ bin/emergeupdate.sh
@@ -3,3 +3,9 @@ sudo nice -n 19 emerge --sync && \
 sudo nice -n 19 emerge --update --deep --with-bdeps=y --newuse @world && \
 sudo nice -n 19 emerge --depclean && \
 sudo nice -n 19 revdep-rebuild
+
+if [ -d "/usr/portage/packages" ] ; then
+    sudo nice eclean -C -q packages 
+fi
+
+sudo nice eclean -C -q -d -t1w distfiles
