a849f02fc71efc6c75cd6ccd442fdd4eb2351e0f
kingkongmok <kingkongmok@gmail.com>
2015-01-12 09:09:00 +0800

    succeed

--- bin/office_proxylogin.sh
+++ bin/office_proxylogin.sh
@@ -21,10 +21,7 @@ set -o nounset                              # Treat unset variables as an error
 [ -r /etc/default/locale ] && . /etc/default/locale
 [ -n "$LANG" ] && export LANG
 
-set -x 
-exec 1>> /tmp/office_proxylogin.sh_cront.log 2>&1
-
 while true ; do
-    ping -q -c 1 10.0.0.8 &>/dev/null && curl "http://10.0.0.8/login/" -H "Pragma: no-cache" -H "Accept-Encoding: gzip, deflate, sdch" -H "Accept-Language: en-US,en;q=0.8,zh-TW;q=0.6,zh;q=0.4,zh-CN;q=0.2" -H "Authorization: Basic bW9xaW5ncWlhbmc6bHRhWWZNTDJOM1J2Mg==" -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Cache-Control: no-cache" -H "Referer: http://10.0.0.8/redirect/?www.google.com/" -H "Proxy-Connection: keep-alive" --compressed && break
+    ping -q -c 1 10.0.0.8 &>/dev/null && curl "http://10.0.0.8/login/" -H "Pragma: no-cache" -H "Accept-Encoding: gzip, deflate, sdch" -H "Accept-Language: en-US,en;q=0.8,zh-TW;q=0.6,zh;q=0.4,zh-CN;q=0.2" -H "Authorization: Basic bW9xaW5ncWlhbmc6bHRhWWZNTDJOM1J2Mg==" -H "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36" -H "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8" -H "Cache-Control: no-cache" -H "Referer: http://10.0.0.8/redirect/?www.google.com/" -H "Proxy-Connection: keep-alive" --compressed > /dev/null && break 
     sleep 2 
 done
