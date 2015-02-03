55d84222cac3e5f3a5347cc9122e7a1c34478980
kingkongmok <kingkongmok@gmail.com>
2015-02-03 14:29:19 +0800

    remove useless txt

--- bin/2array.pl
+++ bin/2array.pl
@@ -21,11 +21,9 @@
 use strict;
 use warnings;
 
-open F2A, "<", "/home/kk/workspace/perl/2array.txt";
-
 my%k;
 my%h;
-while ( <F2A> ) {
+while ( <DATA> ) {
     (my$c1, my$c2)=split;
     if ( $k{$c1}++ ) {
         $h{$c1}=$h{$c1} > $c2? $h{$c1} : $c2 ;
@@ -39,3 +37,34 @@ while ( (my$k,my$v)=each%h ) {
     print "$k\t$v\n" ;
 }
     
+__DATA__
+b   0
+c   1
+d   2
+e   3
+f   4
+g   5
+h   6
+i   7
+j   8
+k   9
+b   3
+c   4
+d   5
+e   6
+f   7
+g   8
+h   9
+i   10
+j   11
+k   12
+b   2
+c   3
+d   4
+e   5
+f   6
+g   7
+h   8
+i   9
+j   10
+k   11
--- bin/3column.pl
+++ bin/3column.pl
@@ -54,7 +54,7 @@ sub make_fh_aoa {
 } ## --- end sub make_fh_aoa
 
 #my %hoa = make_aoa_hoa(@aoa) ;
-my %hoa = make_fh_aoa(<$fh>) ;
+my %hoa = make_fh_aoa(<DATA>) ;
 
 
 
@@ -63,3 +63,35 @@ my %hoa = make_fh_aoa(<$fh>) ;
 use Data::Dumper;
 print Dumper(\%hoa);
 
+__DATA__
+b   0   0
+c   1   1
+d   2   2
+e   3   3
+f   4   4
+g   5   5
+h   6   6
+i   7   7
+j   8   8
+k   9   9
+b   3   3
+c   4   4
+d   5   5
+a   5   20
+e   6   6
+f   7   7
+g   8   8
+h   9   9
+i   10  10
+j   11  11
+k   12  12
+b   2   2
+c   3   3
+d   4   4
+e   5   5
+f   6   6
+g   7   7
+h   8   8
+i   9   9
+j   10  10
+k   11  11
