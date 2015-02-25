956ab20bb4cea42913f30f478a3e66eae01710d6
kingkongmok <kingkongmok@gmail.com>
2015-02-25 14:19:02 +0800

    keep going option

--- bin/emergeupdate.sh
+++ bin/emergeupdate.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo nice -n 19 emerge --sync && \
-sudo nice -n 19 emerge --update --deep --with-bdeps=y --newuse @world && \
+sudo nice -n 19 emerge --keep-going --update --deep --with-bdeps=y --newuse @world && \
 sudo nice -n 19 emerge --depclean && \
 sudo nice -n 19 revdep-rebuild
 
