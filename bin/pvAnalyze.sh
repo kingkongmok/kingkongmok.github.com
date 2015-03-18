d3d35d348d417120167fea98a6d7768006cd135f
kingkongmok <kingkongmok@gmail.com>
2015-03-18 16:12:12 +0800

    use base64 to merge pics to html then mail

--- bin/pvAnalyze.sh
+++ bin/pvAnalyze.sh
@@ -3,7 +3,8 @@
 /home/moqingqiang/bin/tomcatPVAnalyze.pl
 /home/moqingqiang/bin/tomcatHistoryAnalyze.pl
 /home/moqingqiang/bin/tomcatSizeAnalyze.pl
+/home/moqingqiang/bin/tomcat200.pl
 
-cat /home/moqingqiang/bin/analyze.html | /home/moqingqiang/local/mutt-1.5.23/bin/mutt -e "set content_type=text/html" -s "mmSdk pv analyze" -a /tmp/tomcatPV.png /tmp/tomcatHistory.png  /tmp/tomcatLogSize.png --  moqingqiang@richinfo.cn
-#cat /home/moqingqiang/bin/analyze.html | /home/moqingqiang/local/mutt-1.5.23/bin/mutt -e "set content_type=text/html" -s "mmSdk pv analyze" --  moqingqiang@richinfo.cn
+for i in /tmp/tomcatHistory.png /tmp/tomcatPV.png /tmp/tomcatLogSize.png /tmp/tomcat200.png  ; do echo -en "<img src=\"data:image/png;base64," `base64 $i` "\"\>\n" ; done > /tmp/pv_mail.txt
 
+/home/moqingqiang/local/mutt-1.5.23/bin/mutt -e "set content_type=text/html" -s "mmSdk pv analyze"  moqingqiang@richinfo.cn <  /tmp/pv_mail.txt
