ca3cc6b23d7b665e523ec9aceedcb5d5bab27be9
kingkongmok <kingkongmok@gmail.com>
2015-03-30 14:28:27 +0800

    tar Pczf to remove boring message while taring the logs; change the mail subject

--- bin/pvAnalyze_now.sh
+++ bin/pvAnalyze_now.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
+SUBJ=${*:-"testing mmSdk pv analyze today"}
+
 /home/moqingqiang/bin/tomcat200_now.pl
 /home/moqingqiang/bin/tomcat400_now.pl
 /home/moqingqiang/bin/tomcat500_now.pl
 /home/moqingqiang/bin/tomcatRespTime_now.pl
 /home/moqingqiang/bin/tomcatMethod_now.pl
 
-find  /home/logs/*_mmlogs/crontabLog/ -name \*log -mtime -1 | xargs tar czf /tmp/pvAnalyze_today.tar.gz 
+find  /home/logs/*_mmlogs/crontabLog/ -name \*log -mtime -1 | xargs tar Pczf /tmp/pvAnalyze_today.tar.gz 
 
 for i in /tmp/tomcat200_now.png /tmp/tomcat400_now.png /tmp/tomcat500_now.png /tmp/tomcat-resp_now.png /tmp/tomcatMethod_now.png ; do echo -en "<img src=\"data:image/png;base64," `base64 $i` "\"\>\n" ; done > /tmp/pv_mail_now.txt
 
-/home/moqingqiang/local/mutt-1.5.23/bin/mutt -e "set content_type=text/html" -s "testing mmSdk pv analyze today" -a /tmp/pvAnalyze_today.tar.gz -- moqingqiang@richinfo.cn  <  /tmp/pv_mail_now.txt
+/home/moqingqiang/local/mutt-1.5.23/bin/mutt -e "set content_type=text/html" -s "$SUBJ" -a /tmp/pvAnalyze_today.tar.gz -- moqingqiang@richinfo.cn  <  /tmp/pv_mail_now.txt
