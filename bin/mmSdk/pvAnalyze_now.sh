be78497d83f5c60e12093a021ec771e1ce13b2d1
kingkongmok <kingkongmok@gmail.com>
2015-04-20 14:36:17 +0800

    pvAnalyze_now.sh enable insert mail content to /tmp/pv_mail_now.txt before print pngs

--- bin/mmSdk/pvAnalyze_now.sh
+++ bin/mmSdk/pvAnalyze_now.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+
+
+#-------------------------------------------------------------------------------
+#  sending mail as /tmp/pv_mail_now.txt and PNGs append on it.
+#-------------------------------------------------------------------------------
+
 SUBJ=${*:-"testing mmSdk pv analyze today"}
 
 /opt/mmSdk/bin/tomcat200_now.pl
@@ -10,6 +16,8 @@ SUBJ=${*:-"testing mmSdk pv analyze today"}
 
 find  /home/logs/*_mmlogs/crontabLog/ -name \*log -mtime -1 | xargs tar Pczf /tmp/pvAnalyze_today.tar.gz 
 
-for i in /tmp/tomcat200_now.png /tmp/tomcat400_now.png /tmp/tomcat500_now.png /tmp/tomcat-resp_now.png /tmp/tomcatMethod_now.png ; do echo -en "<img src=\"data:image/png;base64," `base64 $i` "\"\>\n" ; done > /tmp/pv_mail_now.txt
+echo -e "\n\n<H1>================== logs today below==================</H1>\n\n" >> /tmp/pv_mail_now.txt
+for i in /tmp/tomcat200_now.png /tmp/tomcat400_now.png /tmp/tomcat500_now.png /tmp/tomcat-resp_now.png /tmp/tomcatMethod_now.png ; do echo -en "<img src=\"data:image/png;base64," `base64 $i` "\"\>\n" ; done >> /tmp/pv_mail_now.txt
 
 /opt/mmSdk/local/mutt-1.5.23/bin/mutt -e "set content_type=text/html" -s "$SUBJ" -a /tmp/pvAnalyze_today.tar.gz -- moqingqiang@richinfo.cn  <  /tmp/pv_mail_now.txt
+rm /tmp/pv_mail_now.txt
