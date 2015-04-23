e245a5d8485d2d6a7d36a119b9ef7f84791b267c
kingkongmok <kingkongmok@gmail.com>
2015-04-23 11:31:18 +0800

    bin/mmSdk/compareHistoryPV.pl display fix

--- bin/mmSdk/compareHistoryPV.pl
+++ bin/mmSdk/compareHistoryPV.pl
@@ -131,9 +131,9 @@ foreach my $log ( @logArrays ) {
         my $iterator = 0;
         open my $fho, ">> /tmp/pv_mail_now.txt" || $! ;
         print $fho "<pre>";
-        printf $fho "some errors may occurd today in %02d:00~%02d:00 at <font color='red'><b>%s</b></font>, compare with history\nPV/min last hour's average flow <font color='red'><b>%s%d%%</b></font>:\ntoday %02d:00~%02d:00: <font color='blue'><b>%.2f</b></font>\n", $h, $h+1, $server_ip[ $serverIterator ],$diffString,$diffValue * 100, $h, $h+1,$PV_now;
+        printf $fho "some errors may occurd today in %02d:00~%02d:00 at <font color='red'><b>%s</b></font>, compare with history\nPV/min last hour's average flow <font color='red'><b>%s%d%%</b></font>:\ntoday %02d:00~%02d:00 PV/minute: <font color='blue'><b>%.2f</b></font>\n", $h, $h+1, $server_ip[ $serverIterator ],$diffString,$diffValue * 100, $h, $h+1,$PV_now;
         foreach my $day ( @logDays ) {
-            printf $fho "%s %02d:00~%02d:00: <font color='green'><b>%.2f</b></font>\n",$day,$h, $h+1,$PV_hist_array_ref->[$iterator];
+            printf $fho "%s %02d:00~%02d:00 PV/minute: <font color='green'><b>%.2f</b></font>\n",$day,$h, $h+1,$PV_hist_array_ref->[$iterator];
             $iterator++;
         }
         print $fho "</pre>";
