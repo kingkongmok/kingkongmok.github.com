1037cae201dd6d0c59afc5266934e97a1383d277
kingkongmok <kingkongmok@gmail.com>
2015-05-11 15:43:20 +0800

    checkHttpStatusHourly.pl count error times.

--- bin/mmSdk/checkHttpStatusHourly.pl
+++ bin/mmSdk/checkHttpStatusHourly.pl
@@ -20,21 +20,35 @@
 
 use strict;
 use warnings;
+use POSIX qw/strftime/;
 
 my @files = glob("/home/logs/*/crontabLog/http_error.log");
+# my @files = glob("/tmp/http_error.log*");
 
 my ($dirname, $filename, $tmpfile) = ($1,$3,"/tmp/$3-$$.tmp") if $0 =~ m/^(.*)(\\|\/)(.*)\.([0-9a-z]*)/;
 
-open my $fho , "> $tmpfile";
-chomp(my $timestamp = `date -d -1hour +"%F %T"`);
-$timestamp =~ s/:.*$//;
 
+my $regex = strftime "%F %H", localtime( time() - 60*60 );
+my $last_hour = strftime "%H", localtime( time() - 60*60 );
+my $now_hour = strftime "%H", localtime();
+
+my %httperror;
 foreach my $file ( @files ) {
     open my $fh , "< $file" || die $!;
     while ( <$fh> ) {
-        print $fho $_ if $_ =~ /^$timestamp/ ;
+        if ( $_ =~ $regex ) {
+            my @F = split ;
+            $httperror{$F[2]}++ ;
+        }
     }
     close $fh; 
+}
+
+
+open my $fho , "> $tmpfile";
+printf $fho "there's some http errors occored during %s:00~%s:00:\n", $last_hour, $now_hour;
+while ( my( $server, $errortimes ) = each %httperror ) {
+    printf $fho "%s %s times\n", $server, $errortimes;
 }
 close $fho ; 
 
