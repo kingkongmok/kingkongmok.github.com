20b844ad28468b6adc38b55e683acffbc109f2b6
kingkongmok <kingkongmok@gmail.com>
2015-05-26 17:57:37 +0800

    proxychange get sites from www and fping find the best host

--- bin/proxychange.pl
+++ bin/proxychange.pl
@@ -1,86 +1,43 @@
 #!/usr/bin/env perl 
 #===============================================================================
 #
-#         FILE: proxychange.pl
+#         FILE: test.pl
 #
-#        USAGE: ./proxychange.pl  
+#        USAGE: ./test.pl  
 #
-#  DESCRIPTION: set best proxyserver for SS
+#  DESCRIPTION: 
 #
 #      OPTIONS: ---
 # REQUIREMENTS: ---
 #         BUGS: ---
 #        NOTES: ---
-#       AUTHOR: Kenneth Mok (kk), kingkongmok AT gmail DOT com
+#       AUTHOR: YOUR NAME (), 
 # ORGANIZATION: 
 #      VERSION: 1.0
-#      CREATED: 04/13/2015 10:56:53 AM
+#      CREATED: 05/26/2015 03:13:31 PM
 #     REVISION: ---
 #===============================================================================
 
 use strict;
 use warnings;
 use utf8;
-use Tie::File;
+use lib '/home/kk/bin';
+use KK::VPNLWP;
+use lib '/home/kk/.private';
+use GetPassword;
 
-open my $fh , "/home/kk/Dropbox/Documents/shadowsocks_list.txt" || die $!;
+my $pas = new GetPassword;
 
-my $hostArray = join " ",                   # join " " with first column 
-                map {  s/^(\S+).*/$1/sr }   # get first column 
-                readline($fh);              # get line array
+my $username = $pas->set_vpnso("username");
+my $password = $pas->set_vpnso("password");
+my $firstURL = $pas->set_vpnso("firstURL");
+my $loginURL = $pas->set_vpnso("loginURL");
+my $listURL = $pas->set_vpnso("listURL");
 
-#-------------------------------------------------------------------------------
-#  /usr/sbin/fping
-#  -s   Print cumulative statistics upon exit.
-#  -q   Quiet. Don't show per-probe results, but only the final summary. Also 
-#        don't show ICMP error messages.
-#  -c   n Number of request packets to send to each target.  In this mode, 
-#        a line is displayed for each received response (this can suppressed 
-#        with -q or -Q).  Also, statistics about responses for each target 
-#        are displayed when all requests have been sent (or when interrupted).
-#-------------------------------------------------------------------------------
-my @fpingResult = `sudo fping -s -q -c 10 $hostArray 2>&1` ;
+my $vpn = KK::VPNLWP->new($username, $password, $firstURL, $loginURL,
+    $listURL);
 
+my $content = $vpn->getContent;
+my $host = $vpn->getBestHost;
 
-#-------------------------------------------------------------------------------
-#  [ "fpingResult", "lostRate", "response time" ] ;
-#-------------------------------------------------------------------------------
-my @host_Schwartzian_AOA;              
-
-foreach (@fpingResult) {
-    if ( $_ =~ /^\w/ ) {
-         $_ =~ m{.*/
-                (\d+)           # $1, as lostRate% in INTEGER
-                \%.*?/
-                ([^/]*?)        # $2, as average msec respone time
-                /[^/]*?$}x ; 
-         if ( defined  $1 && defined $2 ) {
-             push @host_Schwartzian_AOA , [ "$_" , "$1", "$2" ];
-         }
-    }
-}
-
-my @SortedHostArray = map { $_->[0] } 
-                      sort {
-                           $a->[1] <=> $b->[1]   # sort the lostRate  
-                           ||
-                           $a->[2] <=> $b->[2]   # then the response time
-                      }
-                      @host_Schwartzian_AOA ;
-
-my $bestProxy = +(                          # choose the 1st 
-                    map {s/^(\S+).*/$1/r}   # first colum for the @SortedHostArrayn
-                    @SortedHostArray        
-                )[0];                      
-chomp ( $bestProxy );
-
-#-------------------------------------------------------------------------------
-#  edit config file
-#-------------------------------------------------------------------------------
-tie my @configArray, 'Tie::File', "/etc/shadowsocks.json" or die ;
-if ( $configArray[1] ) {
-    $configArray[1] =~ s/(?<=:\").*?(?=\",*$)/$bestProxy/;
-};
-untie @configArray;
-
-system("sudo systemctl restart ss-local");
+# print $host
