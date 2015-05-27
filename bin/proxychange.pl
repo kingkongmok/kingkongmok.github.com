d5576b78b4972523fbaae495b3c88aa4c0586c34
kingkongmok <kingkongmok@gmail.com>
2015-05-27 15:51:29 +0800

    proxychange.pl for change proxy setting

--- bin/proxychange.pl
+++ bin/proxychange.pl
@@ -23,21 +23,38 @@ use warnings;
 use utf8;
 use lib '/home/kk/bin';
 use KK::VPNLWP;
+use KK::DNSLWP;
 use lib '/home/kk/.private';
 use GetPassword;
 
-my $pas = new GetPassword;
+sub get_vpn_host {
+    my $pas = new GetPassword;
+    my $username = $pas->set_vpnso("username");
+    my $password = $pas->set_vpnso("password");
+    my $firstURL = $pas->set_vpnso("firstURL");
+    my $loginURL = $pas->set_vpnso("loginURL");
+    my $listURL = $pas->set_vpnso("listURL");
+    my $vpn = KK::VPNLWP->new($username, $password, $firstURL, $loginURL,
+        $listURL);
+    my $content = $vpn->getContent;
+    my $besthost = $vpn->getBestHost;
+    return $besthost;
+}
 
-my $username = $pas->set_vpnso("username");
-my $password = $pas->set_vpnso("password");
-my $firstURL = $pas->set_vpnso("firstURL");
-my $loginURL = $pas->set_vpnso("loginURL");
-my $listURL = $pas->set_vpnso("listURL");
-
-my $vpn = KK::VPNLWP->new($username, $password, $firstURL, $loginURL,
-    $listURL);
-
-my $content = $vpn->getContent;
-my $host = $vpn->getBestHost;
-
+sub get_dns_host {
+    my $besthost = shift;
+    my $pas = new GetPassword;
+    my $firstURL = $pas->set_dns("firstURL");
+    my $loginURL = $pas->set_dns("loginURL");
+    my $loginForm = $pas->set_dns("loginForm");
+    my $modifyURL = $pas->set_dns("modifyURL");
+    my $modifyForm = $pas->set_dns("modifyForm");
+    my $dns = KK::DNSLWP->new($firstURL, $loginURL, $loginForm, $modifyURL,
+        $modifyForm, $besthost);
+    my $content = $dns->getContent;
 # print $host
+}
+
+my $besthost;
+$besthost = get_vpn_host ;
+get_dns_host($besthost) 
