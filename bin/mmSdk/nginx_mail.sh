08cb2fb8ad64d94afdfcb57fc424138cbfd04742
kingkongmok <kingkongmok@gmail.com>
2015-06-15 17:49:53 +0800

    send nginxlog only as attchement

--- bin/mmSdk/nginx_mail.sh
+++ bin/mmSdk/nginx_mail.sh
@@ -9,7 +9,7 @@
 SUBJ=${*:-"testing nginx status"}
 
 
-find  /home/logs/*_mmlogs/crontabLog/ -name \*log -mtime -1 | xargs tar Pczf /tmp/pvAnalyze_today.tar.gz 
+find  /home/logs/*_mmlogs/crontabLog/nginx/ -name \*log -mtime -1 | xargs tar Pczf /tmp/pvAnalyze_today.tar.gz 
 
 echo -e "\n\n<H1>================== logs today below==================</H1>\n\n" >> /tmp/nginx_status_now.txt
 for i in /tmp/nginx*.png ; do echo -en "<img src=\"data:image/png;base64," `base64 $i` "\"\>\n" ; done >> /tmp/nginx_status_now.txt
