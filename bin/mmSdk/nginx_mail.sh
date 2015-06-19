a0c183a2b9bae33ffb6a23b9dc48c397c8b73eec
kingkongmok <kingkongmok@gmail.com>
2015-06-19 10:57:24 +0800

    add requestPerServer png, show servers' diff

--- bin/mmSdk/nginx_mail.sh
+++ bin/mmSdk/nginx_mail.sh
@@ -12,7 +12,7 @@ SUBJ=${*:-"testing nginx status"}
 find  /home/logs/*_mmlogs/crontabLog/nginx/ -name \*log -mtime -1 | xargs tar Pczf /tmp/pvAnalyze_today.tar.gz 
 
 echo -e "\n\n<H1>================== logs today below==================</H1>\n\n" >> /tmp/nginx_status_now.txt
-for i in /tmp/nginxPVHourly.png  /tmp/nginxPVToday.png ; do echo -en "<img src=\"data:image/png;base64," `base64 $i` "\"\>\n" ; done >> /tmp/nginx_status_now.txt
+for i in /tmp/nginxPVHourly.png  /tmp/nginxPVToday.png /tmp/nginxPVPerServerHourly.png /tmp/nginxPVPerServerToday.png; do echo -en "<img src=\"data:image/png;base64," `base64 $i` "\"\>\n" ; done >> /tmp/nginx_status_now.txt
 
 /opt/mmSdk/local/mutt-1.5.23/bin/mutt -e "set content_type=text/html" -s "$SUBJ" -a /tmp/pvAnalyze_today.tar.gz -- moqingqiang@richinfo.cn  <  /tmp/nginx_status_now.txt
 rm /tmp/nginx_status_now.txt
