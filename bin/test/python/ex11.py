27d24892766ed22eef797568efd155b1e5c390c3
kingkongmok <kingkongmok@gmail.com>
2015-09-16 18:04:49 +0800

    fix

--- bin/test/python/ex11.py
+++ bin/test/python/ex11.py
@@ -1,18 +1,19 @@
 # Asking Questions
 # http://learnpythonthehardway.org/book/ex11.html
 
-print "How old are you?",
-age = raw_input()
-print "How tall are you?",
-height = raw_input()
-print "How much do you weigh?",
-weight = raw_input()
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
 
-print "So, you're %r old, %r tall and %r heavy." % (
-    age, height, weight)
 
-# while True:
-#     print "please input your choise"
-#     ans = raw_input()
-#     if ans == "y":
-#         break
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
