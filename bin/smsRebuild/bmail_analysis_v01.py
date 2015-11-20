d47fb9c4b5d6a730edcc5636a99ef0d5bea968b1
kingkongmok <kingkongmok@gmail.com>
2015-11-20 14:16:38 +0800

    autoindent

--- bin/smsRebuild/bmail_analysis_v01.py
+++ bin/smsRebuild/bmail_analysis_v01.py
@@ -5,21 +5,23 @@ import os,sys,re
 import time
 logTime = time.strftime('%Y-%m-%d',time.localtime(time.time()-24*60*60))
 logName = ['/home/logs/smsmw/172.16.210.52/bmail/monitoring.log.'+logTime,\
-           '/home/logs/smsmw/172.16.210.53/bmail/monitoring.log.'+logTime,\
-           '/home/logs/smsmw/172.16.210.54/bmail/monitoring.log.'+logTime]
+        '/home/logs/smsmw/172.16.210.53/bmail/monitoring.log.'+logTime,\
+        '/home/logs/smsmw/172.16.210.54/bmail/monitoring.log.'+logTime]
 #logName = ['/tmp/bmail_monitoring.log']
 setDic =   {'mbox:searchMessages':'总搜索接口',\
-            'mbox:getInformation':'酷版资讯分享内容获取',\
-            'mailAttach:download':'酷版附件下载',\
-            'contact:uploadImage':'用户上传头像（普通）',\
-            'contact:uploadAfterCut':'用户裁剪图片上传为头像',\
-            'contact:uploadImageFromDisk':'用户从网盘上传头像',\
-            'contact:uploadAfterDealImage':'上传base64编码的头像',\
-            }
+        'mbox:getInformation':'酷版资讯分享内容获取',\
+        'mailAttach:download':'酷版附件下载',\
+        'contact:uploadImage':'用户上传头像（普通）',\
+        'contact:uploadAfterCut':'用户裁剪图片上传为头像',\
+        'contact:uploadImageFromDisk':'用户从网盘上传头像',\
+        'contact:uploadAfterDealImage':'上传base64编码的头像',\
+        }
 infoDic = {'SearchContacts':'通信录接口','rm SearchMessages':'RM调用接口'}
-#psDic = {'callPSMainUserInfo':'查询邮箱属性','callPSPersonal':'查询个人信息','callPS1003':'查询配置表','callPS6201':'修改配置表',\
-#         'callPS7001':'获取邮件到达通知设置项','callPS7003':'用户邮件到达通知明细增加','callPS7004':'用户邮件到达通知明细修改',\
-#         'callPS7005':'邮件到达通知明细删除接口','callPS6100':'用户登录历史记录查询'}
+"""
+psDic = {'callPSMainUserInfo':'查询邮箱属性','callPSPersonal':'查询个人信息','callPS1003':'查询配置表','callPS6201':'修改配置表',\
+        'callPS7001':'获取邮件到达通知设置项','callPS7003':'用户邮件到达通知明细增加','callPS7004':'用户邮件到达通知明细修改',\
+        'callPS7005':'邮件到达通知明细删除接口','callPS6100':'用户登录历史记录查询'}
+"""
 statisDic = {}
 statisDic1 = {}
 #statisDic2 = {}
@@ -54,7 +56,7 @@ for log in logName:
                         time = int(requestTime.split('=')[1].strip())
                         if interfaceName in setDic.keys():
                             statisDic.setdefault(interfaceName,{'explain':setDic[interfaceName],\
-                            'total':0,'lt50':0,'lt100':0,'lt150':0,'lt200':0,'lt300':0,'lt500':0,'lt1000':0})['total'] += 1
+                                    'total':0,'lt50':0,'lt100':0,'lt150':0,'lt200':0,'lt300':0,'lt500':0,'lt1000':0})['total'] += 1
                             if time < 50 :
                                 statisDic[interfaceName]['lt50'] += 1
                             elif time >50 and time <= 100:
@@ -70,61 +72,65 @@ for log in logName:
                             elif time >500 and time <= 1000:
                                 statisDic[interfaceName]['lt1000'] += 1
                 #处理mergeInfo接口统计
-                elif re.search(r'RunTime',mergeRunTime):
-                    interfaceName = mergeInfoName.split('=')[1]
-                    time = int(mergeRunTime.split('=')[1].strip())
-                    if interfaceName in infoDic.keys():
-                        statisDic1.setdefault(interfaceName,{'explain':infoDic[interfaceName],\
-        'total':0,'lt50':0,'lt100':0,'lt150':0,'lt200':0,'lt300':0,'lt500':0,'lt1000':0})['total'] += 1
-                        if time < 50 :
-                                statisDic1[interfaceName]['lt50'] += 1
-                        elif time >50 and time <= 100:
-                                statisDic1[interfaceName]['lt100'] += 1
-                        elif time >100 and time <= 150:
-                                statisDic1[interfaceName]['lt150'] += 1
-                        elif time >150 and time <= 200:
-                                statisDic1[interfaceName]['lt200'] += 1
-                        elif time >200 and time <= 300:
-                                statisDic1[interfaceName]['lt300'] += 1
-                        elif time >300 and time <= 500:
-                                statisDic1[interfaceName]['lt500'] += 1
-                        elif time >500 and time <= 1000:
-                                statisDic1[interfaceName]['lt1000'] += 1
-                        #处理常用PS接口统计
-#            elif re.search(r'ProfilesService',profiles):
-#                interfaceName = profilesName.split('=')[1]
-#                time = int(psRunTime.split('=')[1].strip())
-#                if interfaceName in psDic.keys():
-#                    statisDic2.setdefault(interfaceName,{'explain':psDic[interfaceName],\
-#                    'total':0,'lt50':0,'lt100':0,'lt150':0,'lt200':0,'lt300':0,'lt500':0,'lt1000':0})['total'] += 1
-#                    if time < 50 :
-#                        statisDic2[interfaceName]['lt50'] += 1
-#                    elif time >50 and time <= 100:
-#                        statisDic2[interfaceName]['lt100'] += 1
-#                    elif time >100 and time <= 150:
-#                        statisDic2[interfaceName]['lt150'] += 1
-#                    elif time >150 and time <= 200:
-#                        statisDic2[interfaceName]['lt200'] += 1
-#                    elif time >200 and time <= 300:
-#                        statisDic2[interfaceName]['lt300'] += 1
-#                    elif time >300 and time <= 500:
-#                        statisDic2[interfaceName]['lt500'] += 1
-#                    elif time >500 and time <= 1000:
-#                        statisDic2[interfaceName]['lt1000'] += 1
+            elif re.search(r'RunTime',mergeRunTime):
+                interfaceName = mergeInfoName.split('=')[1]
+                time = int(mergeRunTime.split('=')[1].strip())
+                if interfaceName in infoDic.keys():
+                    statisDic1.setdefault(interfaceName,{'explain':infoDic[interfaceName],\
+                            'total':0,'lt50':0,'lt100':0,'lt150':0,'lt200':0,'lt300':0,'lt500':0,'lt1000':0})['total'] += 1
+                    if time < 50 :
+                        statisDic1[interfaceName]['lt50'] += 1
+                    elif time >50 and time <= 100:
+                        statisDic1[interfaceName]['lt100'] += 1
+                    elif time >100 and time <= 150:
+                        statisDic1[interfaceName]['lt150'] += 1
+                    elif time >150 and time <= 200:
+                        statisDic1[interfaceName]['lt200'] += 1
+                    elif time >200 and time <= 300:
+                        statisDic1[interfaceName]['lt300'] += 1
+                    elif time >300 and time <= 500:
+                        statisDic1[interfaceName]['lt500'] += 1
+                    elif time >500 and time <= 1000:
+                        statisDic1[interfaceName]['lt1000'] += 1
+                    #处理常用PS接口统计
+                    """
+           elif re.search(r'ProfilesService',profiles):
+               interfaceName = profilesName.split('=')[1]
+               time = int(psRunTime.split('=')[1].strip())
+               if interfaceName in psDic.keys():
+                   statisDic2.setdefault(interfaceName,{'explain':psDic[interfaceName],\
+                           'total':0,'lt50':0,'lt100':0,'lt150':0,'lt200':0,'lt300':0,'lt500':0,'lt1000':0})['total'] += 1
+                   if time < 50 :
+                       statisDic2[interfaceName]['lt50'] += 1
+                   elif time >50 and time <= 100:
+                       statisDic2[interfaceName]['lt100'] += 1
+                   elif time >100 and time <= 150:
+                       statisDic2[interfaceName]['lt150'] += 1
+                   elif time >150 and time <= 200:
+                       statisDic2[interfaceName]['lt200'] += 1
+                   elif time >200 and time <= 300:
+                       statisDic2[interfaceName]['lt300'] += 1
+                   elif time >300 and time <= 500:
+                       statisDic2[interfaceName]['lt500'] += 1
+                   elif time >500 and time <= 1000:
+                       statisDic2[interfaceName]['lt1000'] += 1
+                    """
     monitLog.close()
 
 #格式化输出
 tempLog = open('/tmp/bmail_analysis.html','a')
 
-#formatTable = '<table width="1130" border="1" align="left" cellpadding="0" cellspacing="0">\n\
-#<tr>\n<td width="220" height="30">接口名称 </td>\n<td width="200">接口说明</td>\n<td width="80">访问总量</td>\n<td width="80">0~50%</td>\n\
-#<td width="80">51~100%</td>\n<td width="80">101~150%</td>\n<td width="80">151~200%</td>\n<td width="80">201~300%</td>\n<td width="80">300~500%</td>\n\
-#<td width="80">500~1000%</td>\n<td width="80">&gt;1000%</td>\n</tr>\n'
+"""
+formatTable = '<table width="1130" border="1" align="left" cellpadding="0" cellspacing="0">\n\
+        <tr>\n<td width="220" height="30">接口名称 </td>\n<td width="200">接口说明</td>\n<td width="80">访问总量</td>\n<td width="80">0~50%</td>\n\
+        <td width="80">51~100%</td>\n<td width="80">101~150%</td>\n<td width="80">151~200%</td>\n<td width="80">201~300%</td>\n<td width="80">300~500%</td>\n\
+        <td width="80">500~1000%</td>\n<td width="80">&gt;1000%</td>\n</tr>\n'
+"""
 
 formatTable = '<table width="1130" border="1" align="left" cellpadding="0" cellspacing="0">\n\
-<tr>\n<td width="220" height="30">接口名称 </td>\n<td width="200">接口说明</td>\n<td width="80">访问总量</td>\n<td width="80">0~50毫秒(%)</td>\n\
-<td width="80">51~100毫秒(%)</td>\n<td width="80">101~150毫秒(%)</td>\n<td width="80">151~200毫秒(%)</td>\n<td width="80">201~300毫秒(%)</td>\n<td width="80">300~500毫秒(%)</td>\n\
-<td width="80">500~1000毫秒(%)</td>\n<td width="80">&gt;1000毫秒(%)</td>\n</tr>\n'
+        <tr>\n<td width="220" height="30">接口名称 </td>\n<td width="200">接口说明</td>\n<td width="80">访问总量</td>\n<td width="80">0~50毫秒(%)</td>\n\
+        <td width="80">51~100毫秒(%)</td>\n<td width="80">101~150毫秒(%)</td>\n<td width="80">151~200毫秒(%)</td>\n<td width="80">201~300毫秒(%)</td>\n<td width="80">300~500毫秒(%)</td>\n\
+        <td width="80">500~1000毫秒(%)</td>\n<td width="80">&gt;1000毫秒(%)</td>\n</tr>\n'
 
 tempLog.write(formatTable)
 #处理setting接口
@@ -148,11 +154,11 @@ for name in statisDic.keys():
     bfbGt1000 = str(round((float(total - (lt50 + lt100 + lt150 + lt200 + lt300 + lt500 + lt1000)))/total*100,2))
 
     table = '<tr><td height="30">%(name)s</td><td>%(explain)s</td><td>%(total)d</td>\
-    <td>%(bfb50)s</td><td>%(bfb100)s</td><td>%(bfb150)s</td><td>%(bfb200)s</td>\
-    <td>%(bfb300)s</td><td>%(bfb500)s</td><td>%(bfb1000)s</td>\
-    <td>%(bfbGt1000)s</td></tr>' %{'name':name,'explain':explain,'total':total,'bfb50':bfb50,\
-    'bfb100':bfb100,'bfb150':bfb150,'bfb200':bfb200,'bfb300':bfb300,\
-    'bfb500':bfb500,'bfb1000':bfb1000,'bfbGt1000':bfbGt1000}
+            <td>%(bfb50)s</td><td>%(bfb100)s</td><td>%(bfb150)s</td><td>%(bfb200)s</td>\
+            <td>%(bfb300)s</td><td>%(bfb500)s</td><td>%(bfb1000)s</td>\
+            <td>%(bfbGt1000)s</td></tr>' %{'name':name,'explain':explain,'total':total,'bfb50':bfb50,\
+            'bfb100':bfb100,'bfb150':bfb150,'bfb200':bfb200,'bfb300':bfb300,\
+            'bfb500':bfb500,'bfb1000':bfb1000,'bfbGt1000':bfbGt1000}
     tempLog.write(table)
 #tempLog.write('<tr><td colspan="12">&nbsp;</td></tr>')
 #处理info接口
@@ -176,11 +182,11 @@ for name in statisDic1.keys():
     bfbGt1000 = str(round((float(total - (lt50 + lt100 + lt150 + lt200 + lt300 + lt500 + lt1000)))/total*100,2))
 
     table = '<tr><td height="30">%(name)s</td><td>%(explain)s</td><td>%(total)d</td>\
-    <td>%(bfb50)s</td><td>%(bfb100)s</td><td>%(bfb150)s</td><td>%(bfb200)s</td>\
-    <td>%(bfb300)s</td><td>%(bfb500)s</td><td>%(bfb1000)s</td>\
-    <td>%(bfbGt1000)s</td></tr>' %{'name':name,'explain':explain,'total':total,'bfb50':bfb50,\
-    'bfb100':bfb100,'bfb150':bfb150,'bfb200':bfb200,'bfb300':bfb300,\
-    'bfb500':bfb500,'bfb1000':bfb1000,'bfbGt1000':bfbGt1000}
+            <td>%(bfb50)s</td><td>%(bfb100)s</td><td>%(bfb150)s</td><td>%(bfb200)s</td>\
+            <td>%(bfb300)s</td><td>%(bfb500)s</td><td>%(bfb1000)s</td>\
+            <td>%(bfbGt1000)s</td></tr>' %{'name':name,'explain':explain,'total':total,'bfb50':bfb50,\
+            'bfb100':bfb100,'bfb150':bfb150,'bfb200':bfb200,'bfb300':bfb300,\
+            'bfb500':bfb500,'bfb1000':bfb1000,'bfbGt1000':bfbGt1000}
     tempLog.write(table)
 #tempLog.write('<tr><td colspan="12">&nbsp;</td></tr>')
 #处理ps接口
@@ -200,11 +206,11 @@ for name in statisDic1.keys():
 #    bfbGt1000 = str(round((float(total - (lt50 + lt100 + lt150 + lt200 + lt300 + lt500 + lt1000)))/total*100,2))
 #    
 #    table = '<tr><td height="30">%(name)s</td><td>%(explain)s</td><td>%(total)d</td>\
-#    <td>%(lt50)d</td><td>%(lt100)d</td><td>%(lt150)d</td><td>%(lt200)d</td>\
-#    <td>%(lt300)d</td><td>%(bfb300)s</td><td>%(bfb500)s</td><td>%(bfb1000)s</td>\
-#    <td>%(bfbGt1000)s</td></tr>' %{'name':name,'explain':explain,'total':total,'lt50':lt50,\
-#    'lt100':lt100,'lt150':lt150,'lt200':lt200,'lt300':lt300,'lt500':lt500,'lt1000':lt1000,\
-#    'bfb300':bfb300,'bfb500':bfb500,'bfb1000':bfb1000,'bfbGt1000':bfbGt1000}
+        #    <td>%(lt50)d</td><td>%(lt100)d</td><td>%(lt150)d</td><td>%(lt200)d</td>\
+        #    <td>%(lt300)d</td><td>%(bfb300)s</td><td>%(bfb500)s</td><td>%(bfb1000)s</td>\
+        #    <td>%(bfbGt1000)s</td></tr>' %{'name':name,'explain':explain,'total':total,'lt50':lt50,\
+        #    'lt100':lt100,'lt150':lt150,'lt200':lt200,'lt300':lt300,'lt500':lt500,'lt1000':lt1000,\
+        #    'bfb300':bfb300,'bfb500':bfb500,'bfb1000':bfb1000,'bfbGt1000':bfbGt1000}
 #    tempLog.write(table)
 tempLog.write('</table>')
 tempLog.close()
