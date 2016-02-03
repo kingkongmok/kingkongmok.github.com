8bf2f37072843e15d026735ae25715ff0ff2904e
kingkongmok <kingkongmok@gmail.com>
2016-02-03 21:51:58 +0800

    youtube-dl download script

---
+++ bin/yDown.sh
@@ -0,0 +1,35 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: yDown.sh
+# 
+#         USAGE: ./yDown.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Kenneth Mok (kk), kingkongmok AT gmail DOT com
+#  ORGANIZATION: 
+#       CREATED: 02/03/2016 20:56
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+echo "please input URL to download, or 'q' to quit:"
+while read line           
+do           
+    if [ "$line" == "q" ] ; then
+        break
+    else
+        timestamp=`date +%s`
+        nohup /home/kk/workspace/youtube-dl/youtube-dl -f h5 "$line" \
+            -o '/home/kk/Downloads/videos/%(title)s.%(ext)s' \
+            > /tmp/youtube-dl.${timestamp}.log 2>&1 &
+        echo "$line is downloading...\n"
+        echo "please input URL to download, or 'q' to quit:"
+    fi
+done
