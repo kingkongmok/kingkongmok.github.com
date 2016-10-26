9def0c94a22c9caa1b2bc1bae28d52d6d0bc4733
kingkongmok <kingkongmok@gmail.com>
2016-10-26 16:12:57 +0800

    force download

--- bin/yDown.sh
+++ bin/yDown.sh
@@ -37,15 +37,76 @@ downUrl()
         fi
 }	# ----------  end of function downUrl  ----------
 
-echo "please input URL to download, or 'q' to quit:"
-while read line           
-do           
-    if [ "$line" == "q" ] ; then
-        break
-    else
-        downUrl $line &
-        echo "please input URL to download, or 'q' to quit:"
-    fi
-done
 
+forceDownUrl()
+{
+    timestamp=`date +%s`
+    echo "$1" >> /tmp/youtube-dl.${timestamp}.log
+    echo "$1" >> /home/kk/Downloads/videos/history.list.txt
+    youtube-dl -F "$1" | perl -nE 'say $& if /(?<=Finished downloading playlist: ).*/' >> /home/kk/Downloads/videos/history.list.txt
+    nohup /home/kk/workspace/youtube-dl/youtube-dl -f "[height < 720]" "$1" \
+        -o '/home/kk/Downloads/videos/%(title)s-%(autonumber)s.%(ext)s' \
+        >> /tmp/youtube-dl.${timestamp}.log 2>&1 
+}	# ----------  end of function downUrl  ----------
+
+
+#===  FUNCTION  ================================================================
+#         NAME:  usage
+#  DESCRIPTION:  Display usage information.
+#===============================================================================
+function usage ()
+{
+    cat <<- EOT
+
+    DESCRIPTION: download video from youtube-dl
+
+    Usage :  ${0##/*/} [-f]
+
+    Options: 
+    -f            Force Download, ignore history.
+    -h|help       Display this message
+
+EOT
+}    # ----------  end of function usage  ----------
+
+#-----------------------------------------------------------------------
+#  Handle command line arguments
+#-----------------------------------------------------------------------
+SourcePath=
+DestinationPath=
+DESCRIPTION=
+ForceDownload=
+
+while getopts "fh" opt
+do
+    case $opt in
+
+        f     )  ForceDownload=1 ;  ;;
+        h|help     )  usage; exit 0   ;;
+
+        \? )  echo -e "\n  Option does not exist : $OPTARG\n"
+            usage; exit 1   ;;
+
+        esac    # --- end of case ---
+    done
+    shift $(($OPTIND-1))
+
+
+    #-------------------------------------------------------------------------------
+    # main()
+    #-------------------------------------------------------------------------------
+    echo "please input URL to download, or 'q' to quit:"
+    while read line           
+    do           
+        if [ "$line" == "q" ] ; then
+            break
+        else
+            if [ "$ForceDownload" ] ; then
+                forceDownUrl $line &
+            else
+                downUrl $line &
+            fi
+            echo "please input URL to download, or 'q' to quit:"
+        fi
+    done
 
