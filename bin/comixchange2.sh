44ecfe2cd374b7f199ee526ad513d011a8f912a7
kingkongmok <kingkongmok@gmail.com>
2017-03-24 20:39:54 +0800

    post

--- bin/comixchange2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash - 
-for i in *; do unzip "$i"; done
-find -type f ! -iregex  ".*\(jpg\|png\)" -delete
-mv MH*/* .
-rm -rf MH*
-for i in *; do cd "$i"; tar cf "$i".tar *; mv "$i".tar ..; cd ..; done
-ls -d */ | xargs rm -rf
-for i in *tar; do comixchange.sh "$i"; done
