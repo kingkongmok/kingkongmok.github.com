1bf1ddd18c41c5cb59f57e85caecc8f55506ec73
kingkongmok <kingkongmok@gmail.com>
2017-07-11 17:19:38 +0800

    add sqlplus and mysql color prompt

---
+++ linux/login.sql
@@ -0,0 +1,4 @@
+-- set sqlprompt "_USER'@'_CONNECT_IDENTIFIER> "
+set sqlprompt "[32;47m&_user@&_connect_identifier[30m>[0;49m "
+-- set sqlprompt "[33m&_user@&_connect_identifier[30m>[0;49m "
+alter session set nls_date_format='yyyy-mm-dd hh24:mi:ss';     
