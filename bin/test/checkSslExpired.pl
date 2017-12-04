d4d6144c9dfb68208f668dafadbc4b0c2979206c
kingkongmok <kingkongmok@gmail.com>
2017-12-04 10:04:33 +0800

    use curl

--- bin/test/checkSslExpired.pl
+++ bin/test/checkSslExpired.pl
@@ -23,12 +23,17 @@ use warnings;
 # use utf8;
 use Data::Dumper;
 use feature 'say';
-
-
-
 use Date::Parse;
 use POSIX "strftime";
-use LWP::UserAgent;
+
+my $expire_alarm_day = 30;
+my @servers = (
+    "test2.cks.com.hk",
+    "ticketing.cotaiwaterjet.com",
+);
+my $now = time;
+
+
 
 #-------------------------------------------------------------------------------
 #  usage
@@ -52,42 +57,41 @@ HELPTEXT
 
 
 
-my $expire_alarm_day = 30;
+sub getSSLExpireDay(){
+    my $server = shift;
+    my $command = "/usr/bin/curl -connect-timeout 10 -m 20 -skv https://$server/ 2>&1"; 
+    my $curlResult = `$command`;
+    if ( $curlResult =~ /expire\s+date:\s*(.*)/i ) {
+        return $1;
+    }
+    else {
+        exec "echo $server SSL info error";
+    }
+};
 
-my $now = time;
 
-my @servers = (
-    "test2.cks.com.hk",
-    "ticketing.cotaiwaterjet.com",
-);
-
-
-
-my $last_expire;
-my $ua = LWP::UserAgent->new(
-    ssl_opts => {
-        SSL_verify_callback => sub {
-            my ($ok, $ctx_store) = @_;
-            my $cert = Net::SSLeay::X509_STORE_CTX_get_current_cert($ctx_store);
-            $last_expire =
-            Net::SSLeay::P_ASN1_TIME_get_isotime(Net::SSLeay::X509_get_notAfter($cert));
-            return $ok;
-        },
-    },
-);
-
-foreach my $server ( @servers ) {
-    $ua->get("https://$server/");
+sub checkSSL(){
+    my $server = shift;
+    my $last_expire =  &getSSLExpireDay($server);
     if ( $last_expire ) {
         $last_expire = str2time $last_expire;
         my $timeFormat = strftime "%F",  localtime($last_expire);
         #
         # print out result.
         say "$server $timeFormat" if $opt_t;
-        #if ( $last_expire - $now < $expire_alarm_day*24*60*60 ) {
         if ( $last_expire - $now < $expire_alarm_day*24*60*60 ) {
             # say "$server expire date is $timeFormat , please check it.";
             say "程序判断 https://$server/ SSL的CA到期时间为 $timeFormat, 请复查。";
         }
     }
+}
+
+
+if ( $opt_h ) {
+    usage;
+    exit 23;
+}
+
+foreach my $server ( @servers ) {
+    &checkSSL($server);
 }
