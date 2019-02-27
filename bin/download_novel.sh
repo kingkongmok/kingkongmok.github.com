b14787ae0a8147c1b7d1aaea73199592fc29b9c5
kingkongmok <kingkongmok@gmail.com>
2019-02-27 19:25:57 +0800

    post

---
+++ bin/download_novel.sh
@@ -0,0 +1,52 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: download_novel.sh
+# 
+#         USAGE: ./download_novel.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Kenneth Mok (kk), kingkongmok AT gmail DOT com
+#  ORGANIZATION: datlet.com
+#       CREATED: 02/21/2019 16:09
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+URL=$1
+TIMESTAMP=$(date +"%Y%m%d-%H%M%S")
+
+
+echo downloading to "/tmp/novel-done/$TIMESTAMP"
+mkdir -p /tmp/novel-done/$TIMESTAMP/
+proxychains -q wget -q -P /tmp/novel-done/$TIMESTAMP --user-agent="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" -r -l1 "$URL"
+proxychains -q wget -q --user-agent="Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)" "$URL" -O /tmp/novel-done/${TIMESTAMP}/name.html
+
+cd /tmp/novel-done/$TIMESTAMP
+enca -L zh -x utf8 name.html
+TITLE=`perl -nE 'say $1 if /\<title\>.*?-(.*)-/' name.html`
+
+cd www*/bbs*/
+enca -L zh -x utf8 *
+rm -f index.php
+mkdir txt
+for i in `grep 送交者 index.php* -l ` ; do cp "$i" txt ; done
+cd txt
+perl-rename 's/.*/sprintf"%03d.txt",++$i/e' * -i
+for i in *txt ; do perl -i.bak -00ne 'while(/\<pre\>(.*)\<\/pre\>/gsm){print $1}' "$i"; done
+cat *.txt > all.txt
+perl -i.bak -pe 's/<font color=#\w+?>\w+?\.\w+<\/font><p><\/p>//g; s/\r\n//g; s/　　/\n　　/g; s/    /\n　　/g' all.txt
+perl -i -ne 'print unless /^[\s　]+\r?$/' all.txt
+cp all.txt /tmp/"$TITLE".txt
+echo /tmp/"$TITLE".txt done
+#mkdir -p /tmp/novel-done/$TIMESTAMP
+#mv www.* /tmp/novel-done/$TIMESTAMP
+#mv -i all.txt /tmp/"$NAME".txt
+#find /tmp/ -name "www\.*\.com" -type d -mtime -1 -exec rm -rf {} \;
+
