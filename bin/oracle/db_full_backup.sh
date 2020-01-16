b04aa31e4d2d7205f0e4be768ba0baa82e661f24
kingkongmok <kingkongmok@gmail.com>
2020-01-17 00:00:03 +0800

    post

--- bin/oracle/db_full_backup.sh
+++ bin/oracle/db_full_backup.sh
@@ -1,8 +1,31 @@
-#! /bin/sh
+#!/bin/sh
 
 # CONFIGURE ARCHIVELOG DELETION POLICY TO APPLIED ON ALL STANDBY;
-# CONFIGURE SNAPSHOT CONTROLFILE NAME TO <SHARESTORAGE>
+# CONFIGURE SNAPSHOT CONTROLFILE NAME TO '+DATA/${db_unique_name}/controlfile/snapcf_${db_unique_name}.f';
 
-source /home/oracle/.bash_profile
-export DATE=$(date +%m%d%y_%H%M%S)
-$ORACLE_HOME/bin/rman target / cmdfile=/home/oracle/bin/db_full_backup.rcv log=/home/oracle/backuplog/${DATE}_full.log
+export ORACLE_HOME=/u01/app/oracle/product/11.2.0/db_1
+export ORACLE_SID=rac1
+export PATH=$ORACLE_HOME/bin:$PATH
+rman target / << __EOF__
+run
+{
+crosscheck backup;
+DELETE noprompt OBSOLETE;
+allocate channel ch1 device type disk format '/home/oracle/backup/%U.bkp';
+allocate channel ch2 device type disk format '/home/oracle/backup/%U.bkp';
+allocate channel ch3 device type disk format '/home/oracle/backup/%U.bkp';
+allocate channel ch4 device type disk format '/home/oracle/backup/%U.bkp';
+backup database;
+backup archivelog all;
+DELETE NOPROMPT ARCHIVELOG ALL COMPLETED BEFORE 'SYSDATE-7';
+release channel ch1;
+release channel ch2;
+release channel ch3;
+release channel ch4;
+}
+ALLOCATE CHANNEL FOR MAINTENANCE DEVICE TYPE DISK;
+CROSSCHECK BACKUPSET;
+DELETE NOPROMPT OBSOLETE;
+EXIT
+__EOF__
+
