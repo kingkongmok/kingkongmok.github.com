45457f49bdfd57f967a6f542baecbd0c7285cfdc
kingkongmok <kingkongmok@gmail.com>
2021-02-20 17:07:24 +0800

    fix

--- bin/comixchange.sh
+++ bin/comixchange.sh
@@ -5,7 +5,7 @@
 # 
 #         USAGE:  ./comixchange.sh foo.rar; for i in *.zip; do comixchange "$i"; done
 # 
-#   DESCRIPTION:  change zip, rar, tar into tar file, and convert the pics into 1450;
+#   DESCRIPTION:  change zip, rar, tar into tar file, 
 # 
 #       OPTIONS:  ---
 #  REQUIREMENTS:  ---
@@ -13,12 +13,11 @@
 #         NOTES:  ---
 #        AUTHOR: kk (kingkongmok@gmail.com), 
 #       COMPANY: 
-#       CREATED: 05/19/2011 09:08:13 AM CST
+#       CREATED: 02/20/2021 09:08:13 AM CST
 #      REVISION:  ---
 #===============================================================================
 
 set -o nounset                              # Treat unset variables as an error
-set -x
 
 # 设置高像素
 #TARGETHI=1450
@@ -26,7 +25,7 @@ TARGETHI=1600
 
 # 设置图片的平均大小
 #TARGETAVGSIZE=360
-TARGETAVGSIZE=500
+TARGETAVGSIZE=350
 
 function usage () {
 cat << EOF
@@ -84,130 +83,128 @@ WORKPATH="${PATHNAME}/convertdone"
 
 
 if [ ! -f "$FILE" ]; then
-	exit 87;
+    exit 87;
 fi
 
 
 if [ ! -d "$WORKPATH"  ] ; then
-	if [ -d "$PATHNAME" ]; then
-		mkdir -p "$WORKPATH"
-	else
-		echo "path not exits."
-		exit 88;
-	fi
+    if [ -d "$PATHNAME" ]; then
+        mkdir -p "$WORKPATH"
+    else
+        echo "path not exits."
+        exit 88;
+    fi
 fi
+
 
 check_exist ()
 {
-	if [ -f "${WORKPATH}/${FILENAME}.tar" ] ; then
-		echo file exist.
-		exit 68
-	fi
+    if [ -f "${WORKPATH}/${FILENAME}.tar" ] ; then
+        echo file exist.
+        exit 68
+    fi
 }	# ----------  end of function check_exist  ----------
 
 
 extract_it ()
 {
-        case $SUFFIX in
-                zip | ZIP)
-			if [ -z "$PASSWORD" ] ; then
-				unzip -nLqa "$FILE" -d "${WORKPATH}/${FILENAME}"
-			else
-				unzip -nLqa -P "$PASSWORD" "$FILE" -d "${WORKPATH}/${FILENAME}"
-			fi
-                        ;;
- 
-                rar | RAR)
-			mkdir "${WORKPATH}/${FILENAME}" -p
-			if [ -z "$PASSWORD" ] ; then
-				unrar x "$FILE" "${WORKPATH}/${FILENAME}"
-			else
-				unrar x -p"$PASSWORD" "$FILE" "${WORKPATH}/${FILENAME}"
-			fi
-                        ;;
+    case $SUFFIX in
+        zip | ZIP)
+            if [ -z "$PASSWORD" ] ; then
+                unzip -nLqa "$FILE" -d "${WORKPATH}/${FILENAME}"
+            else
+                unzip -nLqa -P "$PASSWORD" "$FILE" -d "${WORKPATH}/${FILENAME}"
+            fi
+            ;;
 
-                tar | TAR)
-			mkdir "${WORKPATH}/${FILENAME}" -p
-			cd "${WORKPATH}/${FILENAME}" 
-			tar xf "${PATHNAME}/${FILE}" 
-			cd "$PATHNAME"
-                        ;;
-                *)
-                        echo "can't unpack the file."
-			exit 88;
-                        ;;
+        rar | RAR)
+            mkdir "${WORKPATH}/${FILENAME}" -p
+            if [ -z "$PASSWORD" ] ; then
+                unrar x "$FILE" "${WORKPATH}/${FILENAME}"
+            else
+                unrar x -p"$PASSWORD" "$FILE" "${WORKPATH}/${FILENAME}"
+            fi
+            ;;
+
+        tar | TAR)
+            mkdir "${WORKPATH}/${FILENAME}" -p
+            cd "${WORKPATH}/${FILENAME}" 
+            tar xf "${PATHNAME}/${FILE}" 
+            cd "$PATHNAME"
+            ;;
+        *)
+            echo "can't unpack the file."
+            exit 88;
+            ;;
 
         esac    # --- end of case ---
-}       # ----------  end 
+    }       # ----------  end 
 
 
 resize ()
 {
-	cd "${WORKPATH}/${FILENAME}"
-	#find "${WORKPATH}/${FILENAME}" -type f ! -iregex ".*\.\(bmp\|jpg\|jpeg\|tiff\|tif\|png\)" -exec rm -f "{}" \;
+
+    cd "${WORKPATH}/${FILENAME}"
+
+
+    # delete NOT IMAGE files
+    find . -type f ! -iregex ".*\.\(apng\|avif\|gif\|jfif\|pjpeg\|pjp\|svg\|webp\|bmp\|ico\|cur\|jpg\|jpeg\|tiff\|tif\|png\)$" -exec rm -f "{}" \;
+
 
     # convert \(bmp\|tiff\|tif\|png\) type to jpg
-	OLDIFS=$IFS
-	IFS=$'\n'
-	FI_2_JPG=(`find . -type f -iregex ".*\.\(bmp\|tiff\|tif\|png\)"`)
-	IFS=$OLDIFS
-    if [ "${#FI_2_JPG[@]}" -gt 0 ] ; then
-        for (( CNTR=0; CNTR<${#FI_2_JPG[@]}; CNTR+=1 )); do
-            nice -n 19 mogrify -format jpg "${FI_2_JPG[${CNTR}]}" && \
-            nice -n 19 rm -f "${FI_2_JPG[${CNTR}]}"
-        done
+    find . -type f -iregex ".*\.\(apng\|avif\|gif\|svg\|webp\|bmp\|ico\|cur\|tiff\|tif\|png\)$" -exec sh -c 'mogrify -format jpg "$1" && rm -f "$1" ' sh {} \;
+
+
+    # list all jpg files
+    for i in `find . -type f -size +300k -iregex ".*\.\(jpg\|jpeg\|jfif\|pjpeg\|pjp\)$"` ; do
+
+
+        # change size if too hi
+        if [[ `identify -format "%h" $i` -gt $TARGETHI ]] ; then
+            convert -resize "x$TARGETHI" "$i" "$i"
+        fi
+
+
+        # change quality
+        WIDTH=$(identify -format '%w' $i)
+        HEIGHT=$(identify -format '%h' $i)
+        FILESIZE=$(stat --printf="%s" $i)
+
+        # single page
+        if  [ $HEIGHT -gt $WIDTH ] ; then
+            QUALITY_RATE=$(echo 0.92*${TARGETAVGSIZE}*1024*100/${FILESIZE}|bc )
+        # double page
+        elif [ $HEIGHT -lt $WIDTH ] ; then
+            QUALITY_RATE=$(echo 0.92*${TARGETAVGSIZE}*2*1024*100/${FILESIZE}|bc )
+        fi
+
+
+        if [ $QUALITY_RATE -ge 120 ] ; then
+            continue
+        elif [ $QUALITY_RATE -lt 120 ] && [ $QUALITY_RATE -ge 80 ] ; then
+            convert -quality "80%" "$i" "$i"
+        elif [ $QUALITY_RATE -lt 80 ] && [ $QUALITY_RATE -gt 0 ] ; then
+            convert -quality "${QUALITY_RATE}%" "$i" "$i"
+        fi
         
-    fi
+
+    done
 
 
-	RESIZEPARA=
-	QUALITYPARA=
-	FILEHIGHT=
-	OLDIFS=$IFS
-	IFS=$'\n'
-	files=(`find . -type f -iregex ".*\.\(bmp\|jpg\|jpeg\|tiff\|tif\|png\)"`)
-	IFS=$OLDIFS
-
-	FILENUMB=${#files[@]}
-	TESTFILE0="${files[(("$FILENUMB"/2))]}"
-	TESTFILE1="${files[(("$FILENUMB"/3))]}"
-	TESTFILE2="${files[(("$FILENUMB"/4))]}"
-
-	if [ `convert "$TESTFILE0" -print "%h" /dev/null` -gt ${TARGETHI} -o `convert "$TESTFILE1" -print "%h" /dev/null` -gt ${TARGETHI} -o `convert "$TESTFILE2" -print "%h" /dev/null` -gt ${TARGETHI} ] ; then
-		RESIZEPARA="${TARGETHI}"
-	fi
-
-	if [ -n "$RESIZEPARA" ] ; then
-		for (( CNTR=0; CNTR<${#files[@]}; CNTR+=1 )); do
-			nice -n 19 convert -resize x${RESIZEPARA}\> "${files[${CNTR}]}" "${files[${CNTR}]}";
-			sleep "$WAITTIME";
-		done
-	fi
-
-	TOTALLYSIZE=$(du -s . | awk '{print $1}')
-	AVGSIZE=$(($TOTALLYSIZE/$FILENUMB))
-	QUALITY=$((${TARGETAVGSIZE}*100/${AVGSIZE}))
-	if [ "$AVGSIZE" -gt ${TARGETAVGSIZE} ] ; then
-		QUALITYPARA=" -quality ${QUALITY}%"
-	fi
-
-	if [ -n "$QUALITYPARA" ] ; then
-			for (( CNTR=0; CNTR<${#files[@]}; CNTR+=1 )); do
-				nice -n 19 convert $QUALITYPARA "${files[${CNTR}]}" "${files[${CNTR}]}"
-			done
-	fi
 }	# ----------  end of function do_resize  ----------
 
 tar_it ()
 {
-	cd "${WORKPATH}/${FILENAME}"
-    find -iname Thumbs.db -type f -exec rm -f "{}" \;
-	tar cf "${WORKPATH}/${FILENAME}.tar" *
-	
-	if [ -d "${WORKPATH}/${FILENAME}" ] ; then
-		rm -rf "${WORKPATH}/${FILENAME}"
-	fi
-	
+
+
+    cd "${WORKPATH}/${FILENAME}"
+    tar cf "${WORKPATH}/${FILENAME}.tar" *
+
+    if [ -d "${WORKPATH}/${FILENAME}" ] ; then
+        rm -rf "${WORKPATH}/${FILENAME}"
+    fi
+
+
 }	# ----------  end of function tar_it  ----------
 
 check_exist
