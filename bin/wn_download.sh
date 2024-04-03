42d838d726c98d565759640407529f8ffaa3c4e2
kingkongmok <kingkongmok@gmail.com>
2024-04-04 01:05:25 +0800

    post

--- bin/wn_download.sh
+++ bin/wn_download.sh
@@ -27,7 +27,7 @@ comicDownloadFailure=~/Dropbox/var/log/wn_download/comic.fail
 CURL_HEADER="User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.89 Safari/537.36"
 DOWNLOAD_DIR=~/Downloads/comic
 # --socks5-hostname Use  the specified SOCKS5 proxy (and let the proxy resolve the host name)
-CURL="/usr/bin/curl --socks5-hostname 127.0.0.1:7073"
+CURL="/usr/bin/curl --socks5-hostname 10.255.255.3:7073"
 # CURL="/usr/bin/curl"
 FailureMode=0
 ERRORMAKR=0
@@ -44,7 +44,7 @@ getURL()
     METHODHOSTNAME=`echo $DOWNLOADLINE | perl -ne 'print $1 if /^(.*?)\/\//'`
     # get URL from $1
     #CURL="/usr/bin/curl --socks5-hostname 127.0.0.1:7074"
-    MESSAGE=`$CURL --socks5-hostname 127.0.0.1:7074  -k -H "$CURL_HEADER" -s $DOWNLOADLINE`
+    MESSAGE=`$CURL -k -H "$CURL_HEADER" -s $DOWNLOADLINE`
     MESSAGESIZE=`echo "$MESSAGE" | wc -c`
 
     if [ $MESSAGESIZE -gt 1000 ] ; then
