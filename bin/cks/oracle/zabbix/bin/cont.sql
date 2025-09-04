3aca8e1093f6567d001f301aedebe105ac4d7da1
莫庆强 <kingkongmok@gmail.com>
2025-09-04 20:29:20 +0800

    Delete bin/cks directory

--- bin/cks/oracle/zabbix/bin/cont.sql
+++
@@ -1,4 +0,0 @@
-whenever sqlerror exit failure 
-set verify off echo off feedback off heading off pagesize 0 trimout on trimspool on termout off 
-conn zabbix/5uZGdsftmcPCA
-column retvalue format a15
